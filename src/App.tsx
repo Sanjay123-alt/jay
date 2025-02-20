47a90bbd6a03adc59ca0ea0af0d1bb743e32b43e
Sanjay123-alt <79630709+Sanjay123-alt@users.noreply.github.com>
2025-02-20 16:44:42 +0530

    my first committ

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-primary">
+      <div className="text center">
+        <p className='text-white'>Hey Its me Sandy</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
